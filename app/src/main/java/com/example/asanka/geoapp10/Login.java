214efd4dd029807f8310b708ecf3ce5e97dbe443
Asanka M <asankalk@gmail.com>
2016-10-24 18:44:03 +0800

    Added Dashboard Activity

--- app/src/main/java/com/example/asanka/geoapp10/Login.java
+++ app/src/main/java/com/example/asanka/geoapp10/Login.java
@@ -1,13 +1,30 @@
 package com.example.asanka.geoapp10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(Login.this,Dashboard.class);
+                startActivity(i);
+            }
+        }
+        );
     }
 }
